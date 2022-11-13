8703a3f95b3cc9938c0476f969bce03d946ab6f1
CyrilBaah <cyril.baah@amalitech.org>
2022-11-13 10:38:32 +0000

    review model

--- watchlist/api/serializers.py
+++ watchlist/api/serializers.py
@@ -3,30 +3,14 @@ from watchlist.models import WatchList, StreamPlatform
 
 
 class WatchListSerializer(serializers.ModelSerializer):
-    # length_of_name  = serializers.SerializerMethodField()
     class Meta:
       model = WatchList
       fields = "__all__"
       
       
 class StreamPlatformSerializer(serializers.ModelSerializer):
+  watchlist = WatchListSerializer(many=True, read_only=True)
   class Meta:
     model = StreamPlatform
     fields = "__all__"
       
-    # """Get length of movie name."""
-    # def get_length_of_name(self, object):
-    #     return len(object.name)     
-      
-    # """Validate different between name and description."""
-    # def validate(self, data):
-    #     if data['name'] == data['description']:
-    #         raise serializers.ValidationError("Movie name and description should be different")
-    #     return data
-    
-    # """Validate length for movie name."""
-    # def validate_name(self, value):
-    #     if len(value) < 5:
-    #         raise serializers.ValidationError("Movie name is too short")
-    #     else:
-    #       return value
--- watchlist/api/urls.py
+++ watchlist/api/urls.py
@@ -4,7 +4,7 @@ from watchlist.api.views import WatchListAV, WatchDetails, StreamPlatformAV, Str
 urlpatterns = [
      path('list/', WatchListAV.as_view(), name='watch-list'),
      path('<int:pk>/', WatchDetails.as_view(), name='watch-details'),
-     path('streams/', StreamPlatformAV.as_view(), name="stream-list"),
-     path('streams/<int:pk>/', StreamPlatformDetail.as_view())
+     path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
+     path('stream/<int:pk>/', StreamPlatformDetail.as_view(), name='stream-details')
  ]
  
