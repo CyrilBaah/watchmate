9343e5268f7d0fb2e2d4daf08ed75ee734d039b1
CyrilBaah <cyril.baah@amalitech.org>
2022-11-23 10:34:16 +0000

    registration serializer

--- userapp/serializers.py
+++ userapp/serializers.py
@@ -6,7 +6,26 @@ class RegistrationSerializer(serializers.ModelSerializer):
   
   class Meta:
       model = User
+    #   fields = "__all__"
+      
       fields = ['username', 'email', 'password', 'password2']
       extra_kwargs = {
           'password': { 'write_only': True }
       }
+      
+# def save(self):
+#     password = self.validated_data['password']
+#     password2 = self.validated_data['password2']
+    
+#     """Check if password and password2 are the same"""
+#     if password != password2:
+#         raise serializers.ValidationError({ "error": "Passwords are not the same" })
+    
+#     """Check if email exists"""
+#     if User.objects.filter(email=self.validated_data['email']).exists():
+#         raise serializers.ValidationError({ "error": "Email already exists" })
+    
+#     account = User(email=self.validated_data['email'], username=self.validated_data['username'])
+#     account.set_password(password)
+#     account.save()
+#     return account
--- userapp/views.py
+++ userapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from userapp.serializers import RegistrationSerializer
 
 # Create your views here.
+@api_view(['POST'])
+def registration_view(request):
+    """Register a User"""
+    if request.method == 'POST':
+        # return Response(request.data)
+        serializer = RegistrationSerializer(data=request.data)  
+        return Response(serializer.is_valid()) 
+           
+        # return Response(serializer.initial_data) 
+        # if serializer.is_valid():
+            # serializer.save()
+            # return Response(serializer.data)
+            # return Response(serializer.data)
